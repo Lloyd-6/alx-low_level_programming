1b9baa9fa05438716b28b5994b6610366638b70c
Lloyd-6 <laloydenoch@gmail.com>
2023-03-16 16:47:20 -0700

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-echo 'Programming is like building a multilingual puzzle' | puts "" && echo 0
+#include <stdio.h>
+
+  /**
+   * Main - Entry point
+   *
+   * Return:Always 0 (Success)
+   */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+} 
