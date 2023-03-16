e1b0179a26bf918ea49a8882d891ce0e14cb61bf
Lloyd-6 <laloydenoch@gmail.com>
2023-03-16 13:27:04 -0700

    My commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#!/bin/bash 
+echo "Size of a char: $(sizeof(char)) byte(s)"
+echo "Size of an int: $(sizeof(int)) byte(s)"
+echo "Size of a long int: $(sizeof(long int)) byte(s)"
+echo "Size of a long long int: $(sizeof(long long int)) byte(s)"
+echo "Size of a float: $(sizeof(float)) byte(s)"
+echo "Size of a double: $(sizeof(double)) byte(s)"
+echo "Size of a long double: $(sizeof(long double)) byte(s)"
+echo 0
